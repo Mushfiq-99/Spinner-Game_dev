0dc696f0af5c663578a93989f72dfa57f53fa708
unknown <mh.mushfiq99@gmail.com>
2021-11-01 11:02:56 +0600

    Updated Color wheel

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
     public bool isRotate=false;
-    public float rotationSpeed= 500f;
-    public float multiplier = 0.9f;
+    public float rotationSpeed;
+    public float multiplier;
     public GameObject needle;
 
     // Start is called before the first frame update
@@ -18,9 +19,16 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (rotationSpeed < 1f)
         {
-            rotationSpeed = Random.Range(700f, 1000f);
+            isRotate = false;
+            float z = transform.rotation.eulerAngles.z;
+            ColorPicker(z);
+      
+        }
+        if(!isRotate && Input.GetKeyDown(KeyCode.Space))
+        {
+            rotationSpeed = Random.Range(3700f, 4000f);
             multiplier = Random.Range(0.95f, 0.97f);
             isRotate = true;
         }
@@ -32,5 +40,37 @@ public class Rotate : MonoBehaviour
             transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
             needle.SetActive(true);
         }
+        
+        
+    }
+
+    void ColorPicker(float z)
+    {
+        if (z >= 1f && z <= 60f)
+        {
+            Debug.Log("Orange");
+        }
+        else if (z >= 61f && z <= 120f)
+        {
+            Debug.Log("Yellow");
+        }
+            
+        else if (z >= 121f && z <= 180f)
+        {
+            Debug.Log("Light Green");
+        }
+        else if (z >= 181f && z <= 240f)
+        {
+            Debug.Log("Sky Blue");
+        }
+        else if (z >= 241f && z <= 300f)
+        {
+            Debug.Log("Purple");
+        }
+        else if (z >= 301f && z <= 360f)
+        {
+            Debug.Log("Pink");
+        }
+
     }
 }
